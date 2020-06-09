8f40004d61a80f0717c0879d21f691fad1615819
Diab <emailcodydiab@gmail.com>
2020-06-09 07:28:45 -0500

    encypt and validate password/login

--- models/User.js
+++ models/User.js
@@ -2,7 +2,11 @@ const {Model, DataTypes} = require('sequelize');
 const sequelize = require('../config/connection');
 const bcrypt = require('bcrypt');
 //create user model
-class User extends Model{}
+class User extends Model{
+    checkPassword(loginPw) {
+        return bcrypt.compareSync(loginPw, this.password);
+    }
+}
 
 //define table comumns and config 
 
